18101c2572ea9245c950db5aac4ac26f199a51fb
M.M <reach.wire@pm.me>
2021-08-08 23:16:25 +0300

    Changed font-color in header

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,11 @@
 export const Navigation = (props) => {
     return (
         <nav>
-            <li>Сотрудничество</li>
-            <li>Наши партнеры</li>
-            <li>Отзывы</li>
-            <li>Как начать</li>
-            <li>Контакты</li>
+            <li><a href="#offer">Сотрудничество</a></li>
+            <li><a href="#parnters">Наши партнеры</a></li>
+            <li><a href="#feedback">Отзывы</a></li>
+            <li><a href="#start">Как начать</a></li>
+            <li><a href="#contact">Контакты</a></li>
         </nav>
     )
 }
