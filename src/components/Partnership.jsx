12c7c673425d78206a564a6f67af5f9799381764
M.M <reach.wire@pm.me>
2021-09-22 23:41:09 +0300

    finished cooperation section

--- src/components/Partnership.jsx
+++ src/components/Partnership.jsx
@@ -6,14 +6,24 @@ const defaultProps = {
     description: 'Why drivers choose us',
     blocks: [
         {
-            key: "start",
-            title: "Fast Start",
-            description: "Leave a request right now and start work in a few hours"
+            key: 'start',
+            title: 'Fast Start',
+            description: 'Leave a request right now and start work in a few hours'
         },
         {
-            key: "payments",
-            title: "High Revenue",
-            description: "Get paid with 4% lower commission than other companies"
+            key: 'payments',
+            title: 'High Revenue',
+            description: 'Get paid with 4% lower commission than other companies'
+        },
+        {
+            key: 'schedule',
+            title: 'Convenient Schedule',
+            description: 'Take orders at a convenient time: work from 2 hours a day or on a full basis'
+        },
+        {
+            key: 'support',
+            title: '24 / 7 Support',
+            description: 'Ask questions in online chat or get phone support'
         }
     ]
 }
@@ -26,13 +36,14 @@ export const Partnership = ({props = defaultProps}) => {
                 heading={props.heading}
                 description={props.description}
             />
-            <div className="partnership-content container">
+            <div className="partnership-content">
                 {
                     props.blocks.map(block => {
                         return (
                             <div key={block.key} className="partnership-content__info">
-                                <div className={'info-image__' + block.key}/>
+                                <div className={'info-image info-image__' + block.key}/>
                                 <h4>{block.title}</h4>
+                                <div className='info-border'/>
                                 <p>{block.description}</p>
                             </div>
                         )
