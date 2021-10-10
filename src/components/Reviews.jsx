25364a2a1d839201cfa0bd067f21e1c6e8ff1168
M.M <reach.wire@pm.me>
2021-10-10 15:03:44 +0300

    added feedback template

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -5,7 +5,20 @@ const defaultProps = {
     heading: 'Drivers about our work',
     description: 'Analyze feedback and join to us',
     id: 'reviews',
-    content: 'Content'
+    content: {
+        reviews: [
+            {
+                id: 1,
+                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce cursus erat sed mi rutrum venenatis. Donec eu dapibus libero. Ut et condimentum tellus, eget luctus lectus. Praesent suscipit nisl in dolor lacinia, at ultricies tellus blandit. Mauris convallis velit eu neque ultrices, sit amet pellentesque mi luctus.',
+                author: {
+                    name: 'John Doe',
+                    position: 'Driver',
+                    photo: './images/drivers/1.png'
+                },
+                rating: 5
+            }
+        ]
+    }
 }
 
 export const Reviews = ({props = defaultProps}) => {
@@ -19,7 +32,24 @@ export const Reviews = ({props = defaultProps}) => {
                 <div className='reviews-content'>
                     <div className='reviews-content__border'/>
                     <div className='reviews-content__comments'>
-
+                        {
+                            props.content.reviews.map(review => {
+                                return (
+                                    <div className='comments-item'>
+                                        <div className='comments-item__rating'>
+                                            <span className={'rating-star__' + review.rating}/>
+                                        </div>
+                                        <div className='comments-item__author'>
+                                            <span className='author-name'>{review.author.name}</span>
+                                            <p className='author-position'>{review.author.position}</p>
+                                        </div>
+                                        <div className='comments-item__text'>
+                                            {review.text}
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
                     </div>
                 </div>
             }
