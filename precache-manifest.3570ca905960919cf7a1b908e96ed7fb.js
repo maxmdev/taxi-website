8b30b910fa0b1917b7f9057ed0284f9214f10f54
M.M <reach.wire@pm.me>
2021-11-15 11:39:54 +0200

    Updates

---
+++ precache-manifest.3570ca905960919cf7a1b908e96ed7fb.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abe7cf778ab2dded4d413c57530fec05",
+    "url": "/taxi-website/index.html"
+  },
+  {
+    "revision": "13f87592570b5ab6e6f9",
+    "url": "/taxi-website/static/css/main.6ff6b320.chunk.css"
+  },
+  {
+    "revision": "533d9bb2a99bdbd4adc6",
+    "url": "/taxi-website/static/js/2.ca094478.chunk.js"
+  },
+  {
+    "revision": "77e1b13cea108a173725d7538d8fb564",
+    "url": "/taxi-website/static/js/2.ca094478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c5868646d0a66746a8b",
+    "url": "/taxi-website/static/js/3.d3ec7c47.chunk.js"
+  },
+  {
+    "revision": "13f87592570b5ab6e6f9",
+    "url": "/taxi-website/static/js/main.bd7e224c.chunk.js"
+  },
+  {
+    "revision": "6ab9197f563519a598a8",
+    "url": "/taxi-website/static/js/runtime-main.4cacf7f6.js"
+  },
+  {
+    "revision": "34aae2cd3712d791551d690320237c5d",
+    "url": "/taxi-website/static/media/1.34aae2cd.png"
+  },
+  {
+    "revision": "4c9fe10ffcf03e72ce30086b4cc13a59",
+    "url": "/taxi-website/static/media/2.4c9fe10f.png"
+  },
+  {
+    "revision": "0d9d999df9ccbf9a02da62f9e9a617c7",
+    "url": "/taxi-website/static/media/3.0d9d999d.png"
+  },
+  {
+    "revision": "d000b15ca188b9fd2c93454418d01ce2",
+    "url": "/taxi-website/static/media/Logo.d000b15c.png"
+  },
+  {
+    "revision": "cdf9b0ea2dc3c8fb56658010bdbba786",
+    "url": "/taxi-website/static/media/agreement.cdf9b0ea.svg"
+  },
+  {
+    "revision": "db905c546ade4f57bfb1df5068481f29",
+    "url": "/taxi-website/static/media/arrow-left.db905c54.svg"
+  },
+  {
+    "revision": "a2b8bf5885b641c2611bee29e07251cc",
+    "url": "/taxi-website/static/media/arrow-right.a2b8bf58.svg"
+  },
+  {
+    "revision": "98357e8d3e689f40f3ebef314ac573fc",
+    "url": "/taxi-website/static/media/bm-car.98357e8d.png"
+  },
+  {
+    "revision": "f57e476b5de9f9733011ad09dd5a5ac0",
+    "url": "/taxi-website/static/media/buildings.f57e476b.png"
+  },
+  {
+    "revision": "ef9c9cb128212edc666a940096136393",
+    "url": "/taxi-website/static/media/city.ef9c9cb1.png"
+  },
+  {
+    "revision": "51986459d2a87b55ca798e7805876913",
+    "url": "/taxi-website/static/media/fake-taxi.51986459.png"
+  },
+  {
+    "revision": "5fc2d565714abe24332655677ddb147c",
+    "url": "/taxi-website/static/media/fake-taxi.5fc2d565.png"
+  },
+  {
+    "revision": "b7d25bd0b165f2635c9f405a5e405972",
+    "url": "/taxi-website/static/media/hamburger.b7d25bd0.svg"
+  },
+  {
+    "revision": "169e12fd26ba3eddfc2a7c30d0f18068",
+    "url": "/taxi-website/static/media/handshake.169e12fd.svg"
+  },
+  {
+    "revision": "7fb5c95bd33233d76e0729079a326912",
+    "url": "/taxi-website/static/media/incoming-call.7fb5c95b.svg"
+  },
+  {
+    "revision": "b0c2fbbffb83b37cbc5222cc35f5251e",
+    "url": "/taxi-website/static/media/info.b0c2fbbf.svg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/taxi-website/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "61f287245c91c12c08d9bb572e211add",
+    "url": "/taxi-website/static/media/maxim.61f28724.png"
+  },
+  {
+    "revision": "af80a19d93c0b9ba082873941e36bbb3",
+    "url": "/taxi-website/static/media/maxim.af80a19d.png"
+  },
+  {
+    "revision": "7242b8d4ab2ac1c4e3f475f94815a3bd",
+    "url": "/taxi-website/static/media/money.7242b8d4.svg"
+  },
+  {
+    "revision": "8367d613bf752290bf8deb198d667dbc",
+    "url": "/taxi-website/static/media/mytaxi.8367d613.png"
+  },
+  {
+    "revision": "e642d42966c33072be3c217c69b4af7e",
+    "url": "/taxi-website/static/media/mytaxi.e642d429.png"
+  },
+  {
+    "revision": "fdaf897c2aa16789db59da34dc8a417f",
+    "url": "/taxi-website/static/media/phone.fdaf897c.svg"
+  },
+  {
+    "revision": "2332208bedf231be13d29b238a2c1e1d",
+    "url": "/taxi-website/static/media/prius.2332208b.png"
+  },
+  {
+    "revision": "edeadc7cb23c90d528b4dd9c56f3fb97",
+    "url": "/taxi-website/static/media/quote.edeadc7c.svg"
+  },
+  {
+    "revision": "3967a8f1f751880402e948533b9e901a",
+    "url": "/taxi-website/static/media/rocket.3967a8f1.svg"
+  },
+  {
+    "revision": "fbe8a59a66916f5ac9b04d9dcb35d010",
+    "url": "/taxi-website/static/media/schedule.fbe8a59a.svg"
+  },
+  {
+    "revision": "ab91a7fbc3cda0406a1259f66616c76b",
+    "url": "/taxi-website/static/media/support.ab91a7fb.svg"
+  },
+  {
+    "revision": "386a19ebc1284666c225682598daaf22",
+    "url": "/taxi-website/static/media/taxi-car.386a19eb.png"
+  },
+  {
+    "revision": "a1ea1665339328910cb44b0c7d47dce8",
+    "url": "/taxi-website/static/media/taxi-car.a1ea1665.svg"
+  },
+  {
+    "revision": "12508e663be39491a8aaf6e979a5f5f7",
+    "url": "/taxi-website/static/media/uber.12508e66.png"
+  },
+  {
+    "revision": "c1352c0d8f771bee4269667d3037b37b",
+    "url": "/taxi-website/static/media/uber.c1352c0d.png"
+  },
+  {
+    "revision": "0faa3e0e0e6672cc7d3aba33af9d87fe",
+    "url": "/taxi-website/static/media/user-icon.0faa3e0e.svg"
+  },
+  {
+    "revision": "86a0d85b742c546563d77fe6ab9dbd72",
+    "url": "/taxi-website/static/media/wave.86a0d85b.svg"
+  },
+  {
+    "revision": "d5ebf8754e8975e8e35a729976e2be45",
+    "url": "/taxi-website/static/media/waves.d5ebf875.svg"
+  },
+  {
+    "revision": "0d932f4c76afc8b0c6e09d162e943f80",
+    "url": "/taxi-website/static/media/world-map.0d932f4c.png"
+  },
+  {
+    "revision": "b63aef8fc48d3c48a2e427209693fecc",
+    "url": "/taxi-website/static/media/yandex.b63aef8f.png"
+  },
+  {
+    "revision": "d402d31cf5b5cb4f0cd97e50d845c620",
+    "url": "/taxi-website/static/media/yandex.d402d31c.png"
+  }
+]);
